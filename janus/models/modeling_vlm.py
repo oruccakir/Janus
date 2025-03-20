101505e5a10c6dd249289c31eb881fbb53862b57
oruccakir <oruccakir2525@gmail.com>
2025-03-20 18:15:55 +0100

    add weights distribution flag and map for deepseek

--- janus/models/modeling_vlm.py
+++ janus/models/modeling_vlm.py
@@ -218,6 +218,9 @@ class MultiModalityCausalLM(MultiModalityPreTrainedModel):
         language_config = config.language_config
         self.language_model = LlamaForCausalLM(language_config)
 
+        self.get_weights_distribution_flag = False
+        self.layers_weights_distribution_map = {}
+
     def prepare_inputs_embeds(
         self,
         input_ids: torch.LongTensor,
